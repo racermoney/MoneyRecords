af557d732a1552d7458f57942043fe7e88a9a7a6
racermomey <79239364+racermomey@users.noreply.github.com>
2021-04-09 19:32:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/moneyrecords/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.moneyrecords;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.web1);
+
+        myWebView.getSettings().setLoadsImagesAutomatically(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        // Configure the client to use when opening URLs
+
+        myWebView.loadUrl("https://linktr.ee/joemerivan16");
+    }
+}
